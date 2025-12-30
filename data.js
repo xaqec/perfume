be06e5968920ad6f83565a24b3463ed91a724604
xaqec <162811352+xaqec@users.noreply.github.com>
2025-12-30 20:32:44 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const perfumes = [
+  {
+    name: "YSL Libre",
+    gender: "kadın",
+    season: "kış",
+    notes: ["vanilya"]
+  },
+  {
+    name: "Dior Sauvage",
+    gender: "erkek",
+    season: "yaz",
+    notes: ["ferah"]
+  },
+  {
+    name: "Tom Ford Oud Wood",
+    gender: "erkek",
+    season: "kış",
+    notes: ["odunsu"]
+  }
+];
